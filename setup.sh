23928d5c8d374eac74e29399067f7dbebe46735d
ampaosiri <111080783+ampaosiri@users.noreply.github.com>
2025-05-23 18:00:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ cd vpn-noui
 # (Optional) Copy a specific setup script to /root if needed
 # sudo cp setup-openvpn-2fa.sh /root/
 
-echo "Setting execute permissions on all .sh files..."
-chmod +x *.sh
+#echo "Setting execute permissions on all .sh files..."
+#chmod +x *.sh
 
 
-#echo "Searching for pam_google_authenticator.so..."
-#find / -name pam_google_authenticator.so 2>/dev/null
+echo "Searching for pam_google_authenticator.so..."
+find / -name pam_google_authenticator.so 2>/dev/null
 
 # Optional: Manual edit step (comment out to automate)
 # echo "Please modify 'openvpn.pam.template' as needed, then save and exit nano."
